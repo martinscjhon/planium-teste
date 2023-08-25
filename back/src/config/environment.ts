3109fe7dc4fef629a6ce4f1507e445233232d7e3
jhonatan <martins.cjhon@gmail.com>
2023-08-25 15:04:38 -0300

    ajustando gitignore

--- back/src/config/environment.ts
+++ back/src/config/environment.ts
@@ -1,9 +1,7 @@
 export class EnvironmentConfig {
   public static Port: number;
-  public static AccessOrigin: string;
 
   public static async register() {
     EnvironmentConfig.Port = 8080;
-    EnvironmentConfig.AccessOrigin = "http://localhost:5173/";
   }
 }
